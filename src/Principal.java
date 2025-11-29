787b53013465ca9481a2ae711e8b46a534825de8
Franco Romay <romayfranco3@gmail.com>
2025-11-29 18:57:43 -0300

    Primer commit: Estructura inicial del conversor

---
+++ src/Principal.java
@@ -0,0 +1,22 @@
+public class Principal {
+    public static void main(String[] args) {
+//        En este emocionante desafío de programación, te invitamos a construir tu propio Conversor de Monedas.
+//
+//        Aprenderás a realizar solicitudes a una API de tasas de cambio, a manipular datos JSON y, finalmente, a filtrar y mostrar las monedas de interés.
+//
+//                Los pasos para completar este desafío se detallarán a continuación y estarán disponibles en la sección adyacente:
+//
+//        Configuración del Ambiente Java;
+//
+//        Creación del Proyecto;
+//
+//        Consumo de la API;
+//
+//        Análisis de la Respuesta JSON;
+//
+//        Filtro de Monedas;
+//
+//        Exibición de Resultados a los usuarios;
+
+    }
+}
